ba40026a0a5db6af4ca5016dbaebc68091e25523
pejovskis <123310041+pejovskis@users.noreply.github.com>
2023-08-14 21:09:42 +0200

    Add files via upload

--- Model/Article.php
+++ Model/Article.php
@@ -1,48 +1,62 @@
 <?php
 
 namespace OxidEsales\CustomPriceReduction\Model;
-// Main Controller
+
 use \OxidEsales\CustomPriceReduction\Controller\MainController;
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Facade\ModuleSettingServiceInterface;
 
 class Article extends \OxidEsales\Eshop\Application\Model\Article
 {
 
-    // Activate the module to reduce the price of the from-yaml-parsed ArtNum
-    public static function onActivate() : void
+    // Override getPrice() function. Retrieve the Admin-given Article-Number and reduce its price.
+    public function getPrice($dAmount = 1)
     {
-        // Initialize the main Controller
-        $mainController = new MainController();
+        // Get the original price
+        $originalPrice = parent::getPrice($dAmount);
 
-        // Retrieve the ArtNum from the yaml Array
-        $sReducedProductArtNum = $mainController->getYamlArtnumValue();
+        // Create mainController instance, retrieve the Article-Number and initialise the final productId
+        $mainController = oxNew(MainController::class);
+        $artNum = $mainController->retrieveArtikelNum();
+        $productId = MainController::getProductIdByArtNum($artNum);
 
-        // Initialize the product object
-        $product = oxNew(self::class);
+        // If there is a product with ID matching the $productId -> reduce its price by the given percent.
+        if ($this->getId() == $productId) {
+            // Retrieve the reduction factor from the module settings
+            $container = ContainerFactory::getInstance()->getContainer();
+            $moduleSettingService = $container->get(ModuleSettingServiceInterface::class);
 
-        // Get the product id using its ArtNum
-        $productId = $mainController->getProductIdByArtNum($sReducedProductArtNum);
+            // Get the value in the 'reduction-percent' field, convert it to string and parse it to float value.
+            // Divide by 100 to get the percent factor.
+            $reductionPercent = ((float)$moduleSettingService->
+                getString('reduction-percent', 'custom_price_reduction')->toString()) / 100;
 
-        // Load & Apply the price reduction
-        $product->load($productId);
-        $mainController->applyPriceReduction($product);
+            // Apply the reduction factor
+            $reductionAmount = $originalPrice->getBruttoPrice() * $reductionPercent;
+            $newPrice = $originalPrice->getBruttoPrice() - $reductionAmount;
+
+            // Apply the new price
+            $newPriceObject = oxNew(\OxidEsales\Eshop\Core\Price::class);
+            $newPriceObject->setPrice($newPrice);
+
+            return $newPriceObject;
+        }
+
+        // Return the original price for every oder Article
+        return $originalPrice;
+
     }
 
-    // Deactivate the module, resetting the original price to the same product
-    public static function onDeactivate() : void
+    // Activate the module
+    public static function onActivate(): void
     {
-        // Initialize the main Controller
-        $mainController = new MainController();
+        $article = new Article();
+        $article->getPrice();
+    }
 
-        // Get the Artnum from the .yaml File
-        $sReducedProductArtNum = $mainController->getYamlArtnumValue();
-
-        // Initialize the product
-        $product = oxNew(self::class);
-        // Get Product's ID
-        $productId = $mainController->getProductIdByArtNum($sReducedProductArtNum);
-        // Load and reset the Product's price
-        $product->load($productId);
-        $mainController->retrievePriceReduction($product);
+    // Deactivate the module
+    public static function onDeactivate(): void
+    {
     }
 
 }
